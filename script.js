148f3a777e17e146f8a4c59f0889e84dc57043be
Anshul <68839359+Anshul0778@users.noreply.github.com>
2022-02-01 10:26:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,15 @@
-$(document).ready(function(){
+const showBtn = document.querySelector('.navBtn');
+const topNav = document.querySelector('.top-nav');
 
-    $('#menu').click(function(){
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
-    });
-
-    $(window).on('scroll load',function(){
-
-      $('#menu').removeClass('fa-times');
-      $('.navbar').removeClass('nav-toggle');
-
-      if($(window).scrollTop() > 0){
-        $('.scroll-top').show();
-      }else{
-        $('.scroll-top').hide();
-      }
-
-      // scroll spy 
-
-      
-    $('section').each(function(){
-
-      let height = $(this).height();
-      let offset = $(this).offset().top - 200;
-      let id = $(this).attr('id');
-      let top = $(window).scrollTop();
-
-      if(top > offset && top < offset + height){
-        $('.navbar ul li a').removeClass('active')
-        $('.navbar').find(`[href="#${id}"]`).addClass('active');
-      }
-
-    });
-
-    });
-
-    // smooth scrolling
-
-    $('a[href*="#"]').on('click',function(e){
-
-      e.preventDefault();
-
-      $('html, body').animate({
-
-        scrollTop : $($(this).attr('href')).offset().top,
-
-      },
-      500,
-      'linear'
-      )
-
-    })
-
+showBtn.addEventListener('click', function(){
+    if(topNav.classList.contains('showNav')){
+        topNav.classList.remove('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-bars"></i>';
+    } else {
+        topNav.classList.add('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-times"></i>';
+    }
 });
+
+/* Lightbox */
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
